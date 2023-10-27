b486666376d4a72e4e66788a314c7ddbd4a1e627
AMartindale98 <amartindale98@gmail.com>
2023-10-27 14:10:52 -0400

    Display word guessed letters

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ const updateText = function (placeholder) {
   wordProgress.innerText = str;
 };
 
+updateText(word);
+
 const validateInput = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
   if (input === "") {
@@ -36,11 +38,45 @@ const makeGuess = function (letter) {
     message.innerText = "You've already guessed that letter! Please try again";
   } else {
     guessedLetters.push(letter);
+    showGuessedLetters();
+    updateWordInProgress(guessedLetters);
   }
-  console.log(guessedLetters);
+  // console.log(guessedLetters);
 };
 
-updateText(word);
+const showGuessedLetters = function () {
+  ul.innerHTML = "";
+  guessedLetters.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerText = `${item}`;
+    ul.append(li);
+  });
+};
+
+const updateWordInProgress = function (guessedLetters) {
+  //wordUpper suggestion that Skillcrush recommends does not work because the guessedLetter array contains only lowercase letters. Doesn't make sense to convert it to uppercase because in the comparison you would need to then convert it back to lowercase to ensure it matches.
+  const wordArray = word.split("");
+  console.log(wordArray);
+  const revealWord = [];
+  for (const singleLetter of wordArray) {
+    if (guessedLetters.includes(singleLetter)) {
+      revealWord.push(singleLetter.toUpperCase());
+    } else {
+      revealWord.push("●");
+    }
+  }
+  console.log(revealWord);
+  wordProgress.innerText = revealWord.join("");
+  checkIfWon();
+};
+
+const checkIfWon = function () {
+  if (wordProgress.innerText === word.toUpperCase()) {
+    message.classList.add("win");
+    message.innerHTML =
+      "<p class='highlight'>You guessed correct the word! Congrats!</p>";
+  }
+};
 
 guessButton.addEventListener("click", function (e) {
   e.preventDefault();
